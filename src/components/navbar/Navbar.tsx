d07ec85cc244bbc2c0d016ae6a15113d72bda929
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-23 20:42:11 -0300

    feat(navbar): add new Navbar component
    This commit introduces the Navbar component to the application, which includes navigation links for Home, Produtos, Categorias, and Cadastrar Categoria within a styled container using Tailwind CSS classes. The 'Infinity Games' brand name is also added as part of the navbar branding. Note that some links like 'Produtos' and 'Sair' are not yet implemented with react-router-dom 'Link' components and may need further development to function correctly.

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+ 
+
+  return (
+    <>
+     <div className='w-full bg-indigo-800 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <Link to='/home' className='text-2xl font-bold uppercase'>Infinity Games</Link>
+
+            <div className='flex gap-4'>
+             <Link to='/home' className='hover:underline'>Home</Link>
+              <div className='hover:underline'>Produtos</div>
+              <Link to='/categorias' className='hover:underline'>Categorias</Link>
+              <Link to='/cadastrarcategorias' className='hover:underline'>Cadastrar Categoria</Link>
+              <div className='hover:underline'>Sair</div>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
